5c8983fa388c30461890155aca00bd948f84af32
taylorgonz <hello@taylorgonz.com>
2021-10-24 13:39:07 -0400

    tip calculator init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import logo from "./images/logo.svg"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="img">
+      <img className='logo' src={logo}/>
+      </div>
+     
+        <section className="mainCard">
+          <section className="input">
+
+          </section>
+          <section className="">
+
+          </section>
+
+        </section>
+        
+    </div>
+  );
+}
+
+export default App;ß
